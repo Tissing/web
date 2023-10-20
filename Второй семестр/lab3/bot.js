7dcde660f3bc342ccc2a37a77c0b0571f80c7cdb
Tissing <shibanov-slava@mail.ru>
2023-10-20 23:01:48 +1100

    init lab3

---
+++ Второй семестр/lab3/bot.js
@@ -0,0 +1,38 @@
+const TelegramBot = require("node-telegram-bot-api");
+const token = "6334469629:AAEDz2k1Sp4vOJw20hTzaE6w6Ade5Wgm-Rk";
+
+const bot = new TelegramBot(token, {
+    polling: {
+        interval: 300,
+        autoStart: true
+    },
+});
+
+bot.on("polling_error", err => console.log(err.message.toString()));
+bot.on('text', async msg => {
+    if (msg.text == "/start")
+    {
+        await bot.sendMessage(msg.chat.id, "Вы запустили бота");
+        await bot.sendMessage(msg.chat.id, "Бот возвращает json вашего сообщения.");
+    }
+    else{
+        let json = `{
+    message_id: ${msg.message_id},
+    from: {
+        id: ${msg.from.id},
+        first_name: ${msg.from.first_name},
+        username: ${msg.from.username},
+        language_code: ${msg.from.language_code}
+        },
+    chat: {
+        id: ${msg.chat.id},
+        first_name: ${msg.chat.first_name},
+        username: ${msg.chat.username},
+        type: ${msg.chat.type}
+        },
+    date: ${msg.date},
+    text: ${msg.text}
+}`
+        await bot.sendMessage(msg.chat.id, `Вот json твоего сообщения: \n ${json}`);
+    }
+});
